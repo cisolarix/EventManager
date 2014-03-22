a79541a47fd62191e0923b6cde8b9ffd0cd6185d
Yanming Deng <cisolarix@gmail.com>
2014-03-22 21:01:28 +0800

    finish

--- event_manager.rb
+++ event_manager.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require 'sunlight/congress'
+require 'erb'
 
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
@@ -7,11 +8,29 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, "0")[0..4]
 end
 
+def legislators_by_zipcode(zipcode)
+  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+  # legislators.map {|l| "#{l.first_name} #{l.last_name}"}
+end
+
+def save_thank_you_letters(id,form_letter)
+  Dir.mkdir('output') unless Dir.exists?('output')
+  filename = "output/thanks_#{id}.html"
+  File.open(filename, 'w') do |f|
+    f.puts form_letter
+  end
+end
+
 content = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol if File.exists? "event_attendees.csv"
+
+template_letter = File.read "form_letter.erb"
+erb_template = ERB.new template_letter
+
 content.each do |row|
+  id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode row[:zipcode]
-  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
-  legislators_names = legislators.map {|l| "#{l.first_name} #{l.last_name}"}
-  puts "#{name}\t\t\t#{zipcode} #{legislators_names}"
+  legislators = legislators_by_zipcode(zipcode)
+  form_letter = erb_template.result(binding)
+  save_thank_you_letters(id, form_letter)
 end
